6102b4b3f68a860a96e45719f48ca832543c181f
Diego Lugo <diego.lugo.06@gmail.com>
2015-11-03 16:43:20 -0500

    Finalize Stripe integration

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,10 +1,12 @@
 class ChargesController < ApplicationController
+  before_action :set_cart
+
   def new
   end
 
   def create
     # Amount in cents
-     
+
 
     customer = Stripe::Customer.create(
       email: 'example@stripe.com',
@@ -13,7 +15,7 @@ class ChargesController < ApplicationController
 
     charge = Stripe::Charge.create(
       customer: customer.id,
-      amount: @total,
+      amount: @cart.total,
       description: 'Rails Stripe customer',
       currency: 'usd'
     )
@@ -24,6 +26,16 @@ class ChargesController < ApplicationController
   end
 
   def paid
-    render plain: params.inspect
   end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_cart
+    @cart = Cart.find_by(id: current_user.id)
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  # def cart_params
+  #   params.require(:cart)
+  # end
 end
